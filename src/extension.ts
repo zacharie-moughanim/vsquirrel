4c579096e552376174e7232b820ad54743bd4a57
ZachMGH <zachariemoughanim@gmail.com>
2026-07-22 11:47:43 +0200

    Fix IPC, now reading server's output JSON object by JSON object

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,9 @@ var ConvertANSIToHTML = require('ansi-to-html');
 
 var convertANSIToHTML = new ConvertANSIToHTML();
 
+// Whether to display debug messages
+const DEBUG_MODE : boolean = true;
+
 // Console channel for debug messages
 let debugChannel : vscode.OutputChannel;
 var client : any;
@@ -23,6 +26,16 @@ const configSquirrelPath : string | undefined = vscode.workspace.getConfiguratio
 
 // The LSP server subprocess
 var lsp_server : ChildProcess;
+var buf_stdout : string = "";
+var buf_stderr : string = "";
+
+// Decorations
+var processingProofColor = "#c3f8d357";
+var processedProofColor = "#00f04857";
+var processedErrorProofColor = "#f0000057";
+var decorationProcessingProof = vscode.window.createTextEditorDecorationType({backgroundColor : processingProofColor});
+var decorationProcessedProof = vscode.window.createTextEditorDecorationType({backgroundColor : processedProofColor});
+var decorationErrorProof = vscode.window.createTextEditorDecorationType({backgroundColor : processedErrorProofColor});
 
 // Position of the beginning of the document, of the last point requested to be processed, and of the last point where the proof was processed. TODO wrap this in an object or something
 const startDocumentPosition = new vscode.Position(0, 0);
@@ -44,7 +57,7 @@ var idx : number = 0;
 /// Sends [msg] to LSP server, computing header on [data]
 function LSPSend(obj : object, withUniqueId : boolean = false) {
 	if (lsp_server.stdin === null) {
-		console.log("LSP server: stdin undefined while sending");
+		console.error("LSP server: stdin undefined while sending");
 	} else {
 		if (withUniqueId) {
 			var obj2 : any = obj; // TODO see if there's no better option
@@ -54,9 +67,58 @@ function LSPSend(obj : object, withUniqueId : boolean = false) {
 		const data : string = JSON.stringify(obj2);
 		const msg_with_header : string = `Content-Length: ${data.length}\r\n\r\n${data}`;
 		lsp_server.stdin.write(`${msg_with_header}`);
-		console.log(`==== Sent ====\n${msg_with_header}\n============`);
+		if (DEBUG_MODE) {
+			console.log(`==== Sent ====\n${msg_with_header}\n============`);
+		}
 	}
 }
+
+/** Manage [data] received on stdout, if [data] represent a single JSON object. */
+function LSPRecvStdout(data : string) : void {
+	const objRcvd = JSON.parse(data);
+	if (Object.hasOwn(objRcvd, "method")) {
+		if (objRcvd.method === "vsquirrel/squirrelProofOutput" && proofPanel !== undefined) {
+			// Display proof state/message from squirrel on proof panel
+			if(objRcvd.kind === "error") {
+				// Display error messages from squirrel on proof panel
+				proofStateErrors = squirrelAsHTML(objRcvd.payload);
+				updateProofStateInWebview(proofPanel);
+				// Highlight the command that triggered the error
+				if (currentEditor !== undefined && lastProcessedPointProofPosition !== undefined && endProofPosition !== undefined) {
+					currentEditor.setDecorations(decorationErrorProof, [new vscode.Range(lastProcessedPointProofPosition, endProofPosition)]);
+				}
+			} else {
+				proofStateErrors = undefined;
+				proofStateMain = squirrelAsHTML(objRcvd.payload);
+				updateProofStateInWebview(proofPanel);
+				if (currentEditor !== undefined && lastProcessedPointProofPosition !== undefined && endProofPosition !== undefined) {
+					// Remove previous processing highlighting and error highlighting, if any; and highlight the command that was just processed
+					currentEditor.setDecorations(decorationErrorProof, []);
+					currentEditor.setDecorations(decorationProcessingProof, []);
+					currentEditor.setDecorations(decorationProcessedProof, [new vscode.Range(startDocumentPosition, endProofPosition)]);
+					// Update positions
+					lastProcessedPointProofPosition = endProofPosition;
+					waitingForProofProcessing = false;
+				}
+			}
+		}
+	}
+
+}
+
+/// Receives data from LSP server TODO
+// function LSPRecv() {
+// 	if (lsp_server.stdout === null) {
+// 		console.error("LSP server: stdout undefined while sending");
+// 	} else {
+// 		// Reading header
+// 		lsp_server.stdout.read()
+// 		// Reading payload
+// 		if (DEBUG_MODE) {
+// 			console.log(`==== Received ====\n${msg_with_header}\n============`);
+// 		}
+// 	}
+// }
 
 /// Returns next valid position after [from] in [doc]. It may add a line if the position is at the end of a line.
 /// Returns [undefined] if [from] is the last valid position in [doc].
@@ -76,7 +138,7 @@ function nextCharacterPosition(doc : vscode.TextDocument, from : vscode.Position
 	}
 }
 
-/// Convert Squirrel's output to text suitable for HTML
+/** Convert Squirrel's output to text suitable for HTML */
 function squirrelAsHTML(body : string) : string {
 	return convertANSIToHTML.toHtml(body).replaceAll("\n", "<br/>");
 }
@@ -109,7 +171,7 @@ function updateProofStateInWebview(panel : vscode.WebviewPanel) : void {
 // Side panel. [undefined] means the proof panel does not exist at the time.
 var proofPanel : vscode.WebviewPanel | undefined = undefined;
 
-/// Find position of next dot in the [doc] from the position [from], ignoring comments e.g. on [(* a sentence. *) Proof.], it returns the position of the second dot.
+/** Find position of next dot in the [doc] from the position [from], ignoring comments e.g. on [(* a sentence. *) Proof.], it returns the position of the second dot. */
 function findNextDot(doc : vscode.TextDocument, from : vscode.Position) : vscode.Position | undefined {
 	var prevChar : string;
 	var curChar : string = "";
@@ -142,14 +204,6 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('VSquirrel is now active.');
 	debugChannel = vscode.window.createOutputChannel("Squirrel Debug", {log : true});
 
-	// Decorations
-	var processingProofColor = "#c3f8d357";
-	var processedProofColor = "#00f04857";
-	var processedErrorProofColor = "#f0000057";
-	var decorationProcessingProof = vscode.window.createTextEditorDecorationType({backgroundColor : processingProofColor});
-	var decorationProcessedProof = vscode.window.createTextEditorDecorationType({backgroundColor : processedProofColor});
-	var decorationErrorProof = vscode.window.createTextEditorDecorationType({backgroundColor : processedErrorProofColor});
-
 	// Finding paths to python and squirrel
 	var pythonPath : string;
 	var squirrelPath : string;
@@ -173,43 +227,51 @@ export function activate(context: vscode.ExtensionContext) {
 	lsp_server = spawn(pythonPath, serverStartCLOptions, { "cwd": server_workdir });
 
 	if (lsp_server.stdout !== null) {
+		lsp_server.stdout.setEncoding("utf8");
 		lsp_server.stdout.on('data', (data : string) => {
-			const objRcvd = JSON.parse(data);
-			if (Object.hasOwn(objRcvd, "method")) {
-				if (objRcvd.method === "vsquirrel/squirrelProofOutput" && proofPanel !== undefined) {
-					// Display proof state/message from squirrel on proof panel
-					if(objRcvd.kind === "error") {
-						// Display error messages from squirrel on proof panel
-						proofStateErrors = squirrelAsHTML(objRcvd.payload);
-						updateProofStateInWebview(proofPanel);
-						// Highlight the command that triggered the error
-						if (currentEditor !== undefined && lastProcessedPointProofPosition !== undefined && endProofPosition !== undefined) {
-							currentEditor.setDecorations(decorationErrorProof, [new vscode.Range(lastProcessedPointProofPosition, endProofPosition)]);
-						}
-					} else {
-						proofStateMain = squirrelAsHTML(objRcvd.payload);
-						updateProofStateInWebview(proofPanel);
-						if (currentEditor !== undefined && lastProcessedPointProofPosition !== undefined && endProofPosition !== undefined) {
-							// Remove previous processing highlighting and error highlighting, if any; and highlight the command that was just processed
-							currentEditor.setDecorations(decorationErrorProof, []);
-							currentEditor.setDecorations(decorationProcessingProof, []);
-							currentEditor.setDecorations(decorationProcessedProof, [new vscode.Range(startDocumentPosition, endProofPosition)]);
-							// Update positions
-							lastProcessedPointProofPosition = endProofPosition;
-							waitingForProofProcessing = false;
+			buf_stdout += data;
+			console.log(`==stdout==\n${data}\n==end stdendout==`);
+			debugChannel.appendLine(data); // .toString() not sure it's useful...
+			// Parsing buffer. It may contain several chunks of the form HEADER\r\nAJSONOBJECT. We read all such chunks and pass them to LSPRecvStdout
+			let stillDataToParse : boolean = true;
+			while (stillDataToParse) {
+				stillDataToParse = false;
+				let contentLength : number | undefined = undefined;
+				const contentlengthFieldTitle = "Content-Length:";
+				const lines_stdout = buf_stdout.split("\n");
+				let readingHeader : boolean = true;
+				let i = 0;
+				for (i = 0; i < lines_stdout.length && readingHeader; ++i) {
+					let line = lines_stdout[i];
+					if (line.trim() === "") {
+						readingHeader = false;
+					}
+					if(line.substring(0, contentlengthFieldTitle.length).toLowerCase() === contentlengthFieldTitle.toLowerCase()) {
+						const splitLine = line.split(":");
+						if (splitLine.length >= 2) { // Otherwise, we wait for more output from LSP server
+							contentLength = parseInt(splitLine[1]);
 						}
 					}
 				}
+				if (contentLength !== undefined) {
+					const rest : string = lines_stdout.filter((v, j) => j >= i).join("\n");
+					if (rest.length >= contentLength) {
+						stillDataToParse = true;
+						LSPRecvStdout(rest.substring(0, contentLength));
+						buf_stdout = rest.substring(contentLength);
+					}
+				}
 			}
-			console.log(`==stdout==\n${data}\n==end stdout==`);
-			debugChannel.appendLine(data.toString());
 		});
 	} else {
 		console.error("LSP server: stdout undefined");
 	}
-	
+
 	if (lsp_server.stderr !== null) {
+		lsp_server.stderr.setEncoding("utf8");
 		lsp_server.stderr.on('data', (data : string) => {
+			// TODO read bit by bit as is already done for stdout.
+			console.error(`==stderr==\n${data}\n==end stderr==`);
 			const objRcvd = JSON.parse(data);
 			if (Object.hasOwn(objRcvd, "method")) {
 				if (objRcvd.method === "vsquirrel/lsperror") {
@@ -222,7 +284,6 @@ export function activate(context: vscode.ExtensionContext) {
 			} else {
 				vscode.window.showErrorMessage(`VSquirrel: LSP server stderr: ${data}`);
 			}
-			console.error(`==stderr==\n${data}\n==end stderr==`);
 		});
 	} else {
 		console.error("LSP server: stderr undefined");
